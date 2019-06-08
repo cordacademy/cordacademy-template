809cb944d6180696ed94e7d1e42c07fc697fca19
Matthew Layton <matthew.layton@live.co.uk>
2019-06-08 23:53:42 +0100

    Added `MockNetworkTest` kotlin documentation.

--- src/test/kotlin/io/cordacademy/test/MockNetworkTest.kt
+++ src/test/kotlin/io/cordacademy/test/MockNetworkTest.kt
@@ -10,6 +10,11 @@ import net.corda.testing.node.TestCordapp
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.BeforeEach
 
+/**
+ * Provides utility for implementing Corda mock network driver based tests.
+ *
+ * @param cordapps A list of cordapps which should be loaded by the mock network.
+ */
 abstract class MockNetworkTest(private vararg val cordapps: String) {
 
     //region initialization members
@@ -28,18 +33,54 @@ abstract class MockNetworkTest(private vararg val cordapps: String) {
 
     //endregion
 
+    /**
+     * Gets the mock network.
+     */
     protected val network: MockNetwork get() = _network
 
+    /**
+     * Gets the default notary's node.
+     */
     protected val notaryNode: StartedMockNode get() = _notaryNode
+
+    /**
+     * Gets participant A's node.
+     */
     protected val nodeA: StartedMockNode get() = _nodeA
+
+    /**
+     * Gets participant B's node.
+     */
     protected val nodeB: StartedMockNode get() = _nodeB
+
+    /**
+     * Gets participant C's node.
+     */
     protected val nodeC: StartedMockNode get() = _nodeC
 
+    /**
+     * Gets the default notary's identity.
+     */
     protected val notaryParty: Party get() = _notaryParty
+
+    /**
+     * Gets participant A's identity.
+     */
     protected val partyA: Party get() = _partyA
+
+    /**
+     * Gets participant B's identity.
+     */
     protected val partyB: Party get() = _partyB
+
+    /**
+     * Gets participant C's identity.
+     */
     protected val partyC: Party get() = _partyC
 
+    /**
+     * Initializes the mock network and participants.
+     */
     @BeforeEach
     fun setup() {
         _network = MockNetwork(
@@ -59,11 +100,20 @@ abstract class MockNetworkTest(private vararg val cordapps: String) {
         _partyC = nodeC.info.singleIdentity()
     }
 
+    /**
+     * Finalizes the mock network.
+     */
     @AfterEach
     fun tearDown() {
         network.stopNodes()
     }
 
+    /**
+     * Runs the mock network and returns a {@code CordaFuture<T>) from the specified function.
+     *
+     * @param function The function which will be run by the network.
+     * @return Returns a {@code CordaFuture<T>) from the specified function.
+     */
     fun <T> run(function: () -> CordaFuture<T>): CordaFuture<T> {
         val result = function()
         network.runNetwork()
